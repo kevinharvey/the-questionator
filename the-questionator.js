2c4870635460a0ef7eac5c4b20cd8176b01a83ce
Kevin Harvey <kevin.harvey@smilecareclub.com>
2014-11-11 00:33:36 -0600

    fixtures need to be in server blocks

--- the-questionator.js
+++ the-questionator.js
@@ -1,24 +1,26 @@
 Questions = new Mongo.Collection('questions');
 
-if (Questions.find().count() === 0) {
-  Questions.insert({
-  	text: 'Why does the sun shine?',
-  	votes: 0
-  });
-  
-  Questions.insert({
-		text: 'If you were a hot dog, and you were starving to death, would you eat yourself?',
-		votes: 0
-	});
-  
-  Questions.insert({
-		text: 'What is the airspeed velocity of an unladen swallow?',
-		votes: 0
-  });
-}
-
-if (Meteor.isClient){
+if (Meteor.isClient) {
   Template.questionsList.helpers({
   	questions: Questions.find(), 
   });
+}
+
+if (Meteor.isServer) {
+  if (Questions.find().count() === 0) {
+    Questions.insert({
+    	text: 'Why does the sun shine?',
+    	votes: 0
+    });
+  
+    Questions.insert({
+  		text: 'If you were a hot dog, and you were starving to death, would you eat yourself?',
+  		votes: 0
+  	});
+  
+    Questions.insert({
+  		text: 'What is the airspeed velocity of an unladen swallow?',
+  		votes: 0
+    });
+  }
 }
