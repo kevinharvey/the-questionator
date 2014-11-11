b3cb02d83c6386ccf4ea6714625ceeec0d333d5f
Kevin Harvey <kevin.harvey@smilecareclub.com>
2014-11-11 00:22:35 -0600

    preload data

--- the-questionator.js
+++ the-questionator.js
@@ -1,21 +1,23 @@
 Questions = new Mongo.Collection('questions');
 
-if (Meteor.isClient){
-  var questionsData = [
-  	{
-  		text: 'Why does the sun shine?',
-  		votes: 0
-  	},
-  	{
-  		text: 'If you were a hot dog, and you were starving to death, would you eat yourself?',
-  		votes: 0
-  	},
-  	{
-  		text: 'What is the airspeed velocity of an unladen swallow?',
-  		votes: 0
-  	}
-  ];
+if (Questions.find().count() === 0) {
+  Questions.insert({
+  	text: 'Why does the sun shine?',
+  	votes: 0
+  });
+  
+  Questions.insert({
+		text: 'If you were a hot dog, and you were starving to death, would you eat yourself?',
+		votes: 0
+	});
+  
+  Questions.insert({
+		text: 'What is the airspeed velocity of an unladen swallow?',
+		votes: 0
+  });
+}
 
+if (Meteor.isClient){
   Template.questionsList.helpers({
   	questions: Questions.find(), 
   });
